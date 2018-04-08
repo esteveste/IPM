3876721d9e30fef449cb4dffb3bbe48c92c10bc8
Bernardo Esteves <esteveste@users.noreply.github.com>
2018-04-08 22:26:56 +0100

    flashing arrow

--- iRave/js/main.js
+++ iRave/js/main.js
@@ -1,6 +1,13 @@
 
 async function init(){
   console.log("lets go");
+  bootAnimation();
+  lockArrow();
+  updateTime();
+
+}
+
+async function bootAnimation(){
   await sleep(2000);
   let boot_title = $("#boot-title");
   let boot_anim = $("#boot-anim");
@@ -13,48 +20,27 @@ async function init(){
     boot_anim.addClass("disabled");
     lockscreen.fadeTo("slow",1);
   });
-
-  //
-  // console.log("awake");
-  // ir = document.getElementById("iravet");
-  // let i = 0;
-  // while(i<1){
-  //   i+=0.01;
-  //   ir.style.opacity = i;
-  //   // console.log(i);
-  //   await sleep(50);
-  // }
-  // await sleep(2000);
-  // fadeout("testbg");
-  // document.getElementById("lock").style.visibility="visible";
-  // ecra.style.width="100%";
-  // ecra.style.height="100%";
-  //   ecra.style.padding="15%";
 }
+function updateTime() {
+  today = new Date();
+  var h = today.getHours();
+  var m = today.getMinutes();
+  if (m < 10) { m = "0" + m };
+  let lock_Time = $("#lock-Time");
+  lock_Time.text(h + ":" + m);
 
-// async function fadeout(el_id){
-//   el = document.getElementById(el_id);
-//   let i = 1;
-//   while(i>0){
-//     i-=0.01;
-//     el.style.opacity = i;
-//     // console.log(i);
-//     await sleep(5);
-//   }
-//   // el.style.backgroundImage = 'url("../st.jpg")';
-//     el.style.backgroundImage = "None";
-//   // el.style.backgroundSize = "contain";
-//   // el.style.visibility="hidden";
-//   ir = document.getElementById("iravet");
-//   ir.innerHTML = "10:24";
-//   while(i<1){
-//     i+=0.01;
-//     el.style.opacity = i;
-//     // console.log(i);
-//     await sleep(20);
-//   }
-// }
-
+  setTimeout(updateTime, 30000);
+}
+async function lockArrow(){
+  let arrow = $("#lock-arrow");
+  if (arrow.css("opacity")=="1") {
+    await sleep(2000);
+    arrow.fadeTo("slow",0);
+  }else{
+    arrow.fadeTo("slow",1);
+  }
+  setTimeout(lockArrow,2000);
+}
 
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
