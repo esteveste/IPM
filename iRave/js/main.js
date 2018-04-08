10efd83a5a9e89618a95da2fef427dd67bccabdb
Bernardo Esteves <esteveste@users.noreply.github.com>
2018-04-08 20:26:17 +0100

    boot anim v1

--- iRave/js/main.js
+++ iRave/js/main.js
@@ -1,15 +1,24 @@
 
 async function init(){
-  await sleep(1000);
-  console.log("awake");
-  ir = document.getElementById("iravet");
-  let i = 0;
-  while(i<1){
-    i+=0.01;
-    ir.style.opacity = i;
-    // console.log(i);
-    await sleep(50);
-  }
+  console.log("lets go");
+
+  let boot_title = $("#boot-title");
+  let boot_anim = $("#boot-anim");
+  boot_title.fadeTo("slow",1);
+  // boot_title.css("opacity",1);
+  await sleep(3000);
+  boot_anim.fadeTo("slow",0,()=>boot_anim.addClass("disabled"));
+
+  //
+  // console.log("awake");
+  // ir = document.getElementById("iravet");
+  // let i = 0;
+  // while(i<1){
+  //   i+=0.01;
+  //   ir.style.opacity = i;
+  //   // console.log(i);
+  //   await sleep(50);
+  // }
   // await sleep(2000);
   // fadeout("testbg");
   // document.getElementById("lock").style.visibility="visible";
@@ -18,28 +27,28 @@ async function init(){
   //   ecra.style.padding="15%";
 }
 
-async function fadeout(el_id){
-  el = document.getElementById(el_id);
-  let i = 1;
-  while(i>0){
-    i-=0.01;
-    el.style.opacity = i;
-    // console.log(i);
-    await sleep(5);
-  }
-  // el.style.backgroundImage = 'url("../st.jpg")';
-    el.style.backgroundImage = "None";
-  // el.style.backgroundSize = "contain";
-  // el.style.visibility="hidden";
-  ir = document.getElementById("iravet");
-  ir.innerHTML = "10:24";
-  while(i<1){
-    i+=0.01;
-    el.style.opacity = i;
-    // console.log(i);
-    await sleep(20);
-  }
-}
+// async function fadeout(el_id){
+//   el = document.getElementById(el_id);
+//   let i = 1;
+//   while(i>0){
+//     i-=0.01;
+//     el.style.opacity = i;
+//     // console.log(i);
+//     await sleep(5);
+//   }
+//   // el.style.backgroundImage = 'url("../st.jpg")';
+//     el.style.backgroundImage = "None";
+//   // el.style.backgroundSize = "contain";
+//   // el.style.visibility="hidden";
+//   ir = document.getElementById("iravet");
+//   ir.innerHTML = "10:24";
+//   while(i<1){
+//     i+=0.01;
+//     el.style.opacity = i;
+//     // console.log(i);
+//     await sleep(20);
+//   }
+// }
 
 
 function sleep(ms) {
