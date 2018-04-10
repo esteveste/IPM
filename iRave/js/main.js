d5cbff211b53efe6875a68d17d3555ca5aace532
Bernardo Esteves <esteveste@users.noreply.github.com>
2018-04-10 17:37:34 +0100

    drag lock

--- iRave/js/main.js
+++ iRave/js/main.js
@@ -1,9 +1,36 @@
 
 async function init(){
   console.log("lets go");
-  bootAnimation();
+  // bootAnimation();
   lockArrow();
   updateTime();
+
+  let lockScreen = $("#lockscreen");
+  lockScreen.css("opacity",1);
+  const maxLockDrag=-225;
+  const minLockDrag=0;
+  lockScreen.draggable({
+        axis: "y",
+        scroll: false,
+        position: 'unset',
+        drag: function (event, ui) {
+            if (ui.position.top > minLockDrag) ui.position.top = minLockDrag;
+            if (ui.position.top < maxLockDrag) ui.position.top = maxLockDrag;
+        },
+        stop: function (event, ui) {
+            if (ui.position.top < maxLockDrag/4) {
+                $(this).animate({
+                    'top': maxLockDrag
+                });
+            }else{
+              $(this).animate({
+                  'top': minLockDrag
+              });
+            }
+            $("#app-screen").addClass("disabled");
+        }
+
+    });
 
 }
 
@@ -23,7 +50,7 @@ async function bootAnimation(){
     boot_anim.addClass("disabled");
     lockscreen.fadeTo("slow",1,()=>black_screen.addClass("disabled"));
   });
-  lockscreen.click(lockScreenUnlock);
+  // lockscreen.click(lockScreenUnlock);
 }
 function updateTime() {
   today = new Date();
